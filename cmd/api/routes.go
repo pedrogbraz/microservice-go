4074559c60ca8898028fe4f53bb424fd4f0bba33
pedrogbraz <pedrobrazdevs@gmail.com>
2025-12-10 01:59:10 -0300

    Add category routes for creating and listing categories

---
+++ cmd/api/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pedrogbraz/microservice-go/cmd/api/controllers"
+	"github.com/pedrogbraz/microservice-go/internal/repositories"
+)
+
+func CategoryRoutes(router *gin.Engine) {
+	// Define category-related routes here
+	{
+		categoryRoutes := router.Group("/categories")
+
+		inMemoryCategoryRepository := repositories.NewInMemoryCategoryRepository()
+
+		categoryRoutes.POST("/", func(ctx *gin.Context) {
+			controllers.CreateCategory(ctx, inMemoryCategoryRepository)
+		})
+
+		categoryRoutes.GET("/", func(ctx *gin.Context) {
+			controllers.ListCategories(ctx, inMemoryCategoryRepository)
+		})
+	}
+}
